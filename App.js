798a8f4a26d0c0e082eb196aaf9f3e0798b3cf0f
Lydia Albert <lydiaalbert@icloud.com>
2023-09-22 18:37:49 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import UpcomingWeather from './src/components/UpcomingWeather'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <UpcomingWeather />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+export default App
